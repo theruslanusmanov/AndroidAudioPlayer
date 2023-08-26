7b23a40e955c2cb2b6d105e2668d8c5c72d663b3
Ruslan Usmanov <ruslanusm6@gmail.com>
2023-08-26 18:04:16 +0300

    force dark theme

--- app/src/main/java/com/emlife/androidaudioplayer/ui/theme/Theme.kt
+++ app/src/main/java/com/emlife/androidaudioplayer/ui/theme/Theme.kt
@@ -39,7 +39,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun AndroidAudioPlayerTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = true, // isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
