42f8ed9f78af178c7e47a33f8061d5d3c580c207
Ruslan Usmanov <ruslanusm6@gmail.com>
2023-08-30 02:09:26 +0300

    add track layout

--- app/src/main/java/com/emlife/androidaudioplayer/ui/components/Track.kt
+++ app/src/main/java/com/emlife/androidaudioplayer/ui/components/Track.kt
@@ -1,24 +1,74 @@
 package com.emlife.androidaudioplayer.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.emlife.androidaudioplayer.R
+import com.emlife.androidaudioplayer.ui.theme.AndroidAudioPlayerTheme
 
 @Composable
 fun Track(onClick: () -> Unit) {
-    Box(
-        Modifier
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
             .background(Color(0xFF1F1F1F))
             .border(width = 1.dp, color = Color.Black)
             .fillMaxWidth()
             .height(80.dp)
             .clickable { onClick() }
-    )
+    ) {
+        // number
+        Text(text = "1")
+
+        // cover
+        Image(
+            painter = painterResource(id = R.drawable.cover),
+            contentDescription = "Cover",
+            alignment = Alignment.Center,
+        )
+
+        // descr
+        Column(modifier = Modifier.weight(1f)) {
+            // title
+            Text(text = "Title Audio")
+
+            // author
+            Text(text = "Title Author", style = TextStyle(color = Color.Gray))
+        }
+
+        // actions button
+        Image(
+            painter = painterResource(id = R.drawable.ic_more_horiz),
+            contentDescription = "Actions",
+            alignment = Alignment.Center,
+        )
+    }
+}
+
+@Preview()
+@Composable
+fun TrackPreview() {
+    AndroidAudioPlayerTheme {
+        Track({})
+    }
 }
--- app/src/main/java/com/emlife/androidaudioplayer/ui/theme/Theme.kt
+++ app/src/main/java/com/emlife/androidaudioplayer/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -14,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.TextStyle
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
@@ -69,6 +71,11 @@ fun AndroidAudioPlayerTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = {
+            ProvideTextStyle(
+                value = TextStyle(color = Color.White),
+                content = content
+            )
+        }
     )
 }
