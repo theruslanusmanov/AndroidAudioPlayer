733684f44077dab0bd55b4d28e3ecb26849faca1
Ruslan Usmanov <ruslanusm6@gmail.com>
2023-08-30 01:42:11 +0300

    add cover img

--- app/src/main/java/com/emlife/androidaudioplayer/ui/components/Cover.kt
+++ app/src/main/java/com/emlife/androidaudioplayer/ui/components/Cover.kt
@@ -1,14 +1,19 @@
 package com.emlife.androidaudioplayer.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.emlife.androidaudioplayer.R
 
 @Composable
 fun Cover() {
@@ -18,5 +23,12 @@ fun Cover() {
             .border(width = 1.dp, color = Color.Black)
             .width(250.dp)
             .height(250.dp)
-    )
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.cover),
+            contentDescription = "Cover",
+            alignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
 }
